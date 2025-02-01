7b1bce91d64df617a5d9d327d97bb90b8824d1b7
Samuel Léobon <76527824+ByggL@users.noreply.github.com>
2025-02-02 00:04:56 +0100

    title modification

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Héritiers de Dumnacus";
+export const SITE_TITLE =
+  "Les Héritiers de Dumnacus — Association de Jeux de rôle à Angers";
 export const SITE_DESCRIPTION = "Welcome to my website!";
