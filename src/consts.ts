dd726068845b063f7656dde4b1856669b2413fdd
MrSc33nsh0t <76527824+MrSc33nsh0t@users.noreply.github.com>
2025-02-05 21:30:19 +0100

    more head shenanigans

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Les Héritiers de Dumnacus — Association de Jeux de rôle à Angers";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+  "Les Héritiers de Dumnacus - Association de Jeux de rôle à Angers";
+export const SITE_DESCRIPTION = "Forum de l'association Les Héritiers de Dumnacus, un club de jeux de rôle à Angers (Maine-et-Loire)";
